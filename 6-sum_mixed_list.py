880c65f87cb1cc5b2a192273c719b5d2ff417a62
Reddeath <frankslayer1@gmail.com>
2024-01-11 22:26:59 +0300

    python

---
+++ 6-sum_mixed_list.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+"""
+Complex types - mixed list
+Write typed-annotated function sum_mixed_list
+that takes mxd_lst integers and floats
+Returns the sum as float
+"""
+from typing import Union, List
+
+
+def sum_mixed_list(mxd_lst: List[Union[int, float]]) -> float:
+    """
+    Typed-annotated function sum_mixed_list
+    """
+    return sum(mxd_lst)
---
+++ 7-to_kv.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+"""
+Complex types - string and int/float to tuple
+Write typed-annotated function to_kv that takes string k and an int OR float v
+Returns the tuple
+"""
+from typing import Union, Tuple
+
+
+def to_kv(k: str, v: Union[int, float]) -> Tuple[str, float]:
+    """
+    Typed-annotated function to_kv
+    """
+    return (k, v * v)
